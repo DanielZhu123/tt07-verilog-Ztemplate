5dfd3bc1a8d0c20fbee92223ee2c8d0d6691288d
DanielZhu123 <168749471+DanielZhu123@users.noreply.github.com>
2024-05-11 19:14:35 +0800

    Update and rename project.v to project.sv

--- src/project.sv
+++ src/project.sv
@@ -15,8 +15,8 @@ module tt_um_DanielZhu123 (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-  reg [9:0] con_ans;
-  reg con_error;
+  logic [9:0] con_ans;
+  logic con_error;
   // All output pins must be assigned. If not used, assign to 0.
   assign uio_oe  = 1;
     
